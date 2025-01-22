4b9c195d3842fb280d637fad820c1093a7f8b3cd
dharshni30 <dharshnirajendran25@gmail.com>
2025-01-22 16:15:39 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,2 @@
-docker pull your-dockerhub-username/your-image:latest
-docker stop your-container || true
-docker rm your-container || true
-docker run -d --name your-container -p 80:80 shaikmustafa/dm
+
+docker run -itd --name cont1 -p 8081:80 shaikmustafa/dm
